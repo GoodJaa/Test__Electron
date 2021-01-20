96aa3e2590e5b1701fcd04c1f303e9c33b2d1864
Golubev Evgeny <limp061289@gmail.com>
2021-01-20 18:41:17 +0300

    first commit

---
+++ stringFilter.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Yor code here ...
+function dscount(text, s1, s2) {
+    const regexp = new RegExp(`${s1}(?=(${s2}))`, 'gi');
+    const result = text.match(regexp);
+    return result !== null ? result.length : 0
+}
+// ... //
+
+// Для удобства можно использовать эти тесты:
+try {
+    test(dscount, ['ab___ab__', 'a', 'b'], 2);
+    test(dscount, ['___cd____', 'c', 'd'], 1);
+    test(dscount, ['de_______', 'd', 'e'], 1);
+    test(dscount, ['12_12__12', '1', '2'], 3);
+    test(dscount, ['_ba______', 'a', 'b'], 0);
+    test(dscount, ['_a__b____', 'a', 'b'], 0);
+    test(dscount, ['-ab-аb-ab', 'a', 'b'], 2);
+    test(dscount, ['aAa', 'a', 'a'], 2);
+
+    console.info("Congratulations! All tests passed.");
+} catch(e) {
+    console.error(e);
+}
+
+// Простая функция тестирования
+function test(call, args, count, n) {
+    let r = (call.apply(n, args) === count);
+    console.assert(r, `Found items count: ${count}`);
+    if (!r) throw "Test failed!";
+}
